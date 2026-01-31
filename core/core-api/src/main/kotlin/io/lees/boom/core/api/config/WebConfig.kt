bf47caa8fc683d0bb1d1ad062c8cc78dc7d57fa4
LJJ <wjdrk70@gmail.com>
2026-02-01 00:37:56 +0900

    fix(config): webconfig

--- core/core-api/src/main/kotlin/io/lees/boom/core/api/config/WebConfig.kt
+++ core/core-api/src/main/kotlin/io/lees/boom/core/api/config/WebConfig.kt
@@ -2,6 +2,7 @@ package io.lees.boom.core.api.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -10,5 +11,10 @@ class WebConfig(
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(userArgumentResolver)
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/docs/**")
+            .addResourceLocations("classpath:/static/docs/")
     }
 }
